71f69c4a5ca717a24d51ad1e8a40ce7fb5ab333d
pedrinzz10 <opedro485@gmail.com>
2026-06-24 00:55:36 -0300

    feat: adiciona springdoc-openapi com esquema de seguranca Bearer (JWT)

---
+++ src/main/java/com/saas/MedStorage_api/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.saas.MedStorage_api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String BEARER_SCHEME = "bearerAuth";
+
+    @Bean
+    public OpenAPI medStorageOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MedStorage API")
+                        .description("SaaS de gestao de pedidos para distribuidor de materiais medicos")
+                        .version("v0.1"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
+                                .name(BEARER_SCHEME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
