6e8c5f65f589fc3633c5535627d70e3af134f0fd
pedrinzz10 <opedro485@gmail.com>
2026-06-26 16:23:20 -0300

    feat: adiciona rate limiting no login (5 req/min por IP, sliding window)

--- src/main/java/com/saas/MedStorage_api/MedStorageApiApplication.java
+++ src/main/java/com/saas/MedStorage_api/MedStorageApiApplication.java
@@ -2,8 +2,10 @@ package com.saas.MedStorage_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MedStorageApiApplication {
 
 	public static void main(String[] args) {
